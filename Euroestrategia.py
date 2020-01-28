07e3bc8342616fb37dda73992f1e40fe4f63c8e0
Javier Ortiz Perez-Jaraiz <javierortiz@edu.uah.es>
2020-01-28 23:46:09 +0100

    Mejorando estrategia y tim

--- Euroestrategia.py
+++ Euroestrategia.py
@@ -12,16 +12,38 @@
 """
 import time
 import math
-from funcionesTyM import calculo_giro_avanzo
+from funcionesTyM import calculo_giro_avanzo_giro
 # Constantes
 # Definir vaso 1 como una tupla
+# Indicar cuantos robots hay
 VASO_1X = 0
 VASO_1Y = 0
-X = 0
-Y = 1
+ACTIVACION_EXPERIMENTOX = -1200
+ACTIVACION_EXPERIMENTOY = 800
+# Estanterias:
+ESTANTERIA_VASOS_1X = -3000
+ESTANTERIA_VASOS_1Y = -1150
+ESTANTERIA_VASOS_2X = -575
+ESTANTERIA_VASOS_2Y = 900
+ESTANTERIA_VASOS_3X = +575
+ESTANTERIA_VASOS_3Y = 900
+ESTANTERIA_VASOS_4X = +3000
+ESTANTERIA_VASOS_4Y = 1150
+ESTANTERIA_VASOS_3X = +575
+ESTANTERIA_VASOS_3Y = 900
+# Bahia
+BAHIA_1X = -400
+BAHIA_1Y = -650
+BAHIA_2X = BAHIA_1X
+BAHIA_2Y = -BAHIA_1Y
+# Puertos:
+PUERTO_SUR_GRISX = -1200
+PUERTO_SUR_GRISY = -300
+PUERTO_SUR_AMARILLOX = -PUERTO_SUR_GRISX
+PUERTO_SUR_AMARILLOY = PUERTO_SUR_GRISY
 
 
-def control(angulo, distancia):
+def control(angulo, distancia, juego):
     """Función que simula el nivel bajo, de momento no hace más que generar delays 
     en función de las distancias recorridas, en el futuro sera capaz de simular 
     enemigos y errores"""
@@ -29,6 +51,7 @@ def control(angulo, distancia):
     wdefecto = 360  # 1Hz
     print("Tiempo que tarda el robot:")
     tiempo = abs((distancia/vdefecto))+abs((angulo/wdefecto))
+    juego.tiempo = juego.tiempo+tiempo
     print(tiempo)
     time.sleep(tiempo)
     return 'A'
@@ -36,7 +59,7 @@ def control(angulo, distancia):
 
 class robot:  # Clase tipo robot donde se almacenan todos los valores insteresantes del propio estado del robot
     def __init__(self, posx, posy, vx, vy):
-        self.pos = [-1500, -700]
+        self.pos = [PUERTO_SUR_GRISX, PUERTO_SUR_GRISY]
         self.bandera = 0
         self.velocidad = [0, 0]
         self.velocidadangular = [0]
@@ -51,13 +74,22 @@ class vasos():  #
         self.vaso4 = vaso4
 
 
+class estanterias():  #
+    def __init__(self, estanteria1, estanteria2, estanteria3, estanteria4):
+        self.estanteria1 = estanteria1
+        self.estanteria2 = estanteria2
+        self.estanteria3 = estanteria4
+        self.estanteria4 = estanteria4
+
+
 class game:  # Clase tipo game, donde se almacenan toda la información del partido y con un vistazo obtienes toda la información
     def __init__(self, posx, posy, vx, vy):
         self.tiempo = 0
         self.puntos = 0
         self.robot = robot(posx, posy, vx, vy)
         self.vasos = vasos(True, True, True, True)
-
+        self.estanterias = estanterias(True, True, True, True)
+        self.experimento = True
         # self.vaso1 = vaso(1, 15, 2, 'r')
         # self.campo
 
@@ -66,29 +98,39 @@ class game:  # Clase tipo game, donde se almacenan toda la información del part
 def planificador(juego):
     if(juego.tiempo <= 100):
         print("Queda tiempo, vamos por la siguiente instruccion")
+        if(juego.experimento):
+            print("Activo el experimento")
+            ejecutor(ACTIVACION_EXPERIMENTOX,
+                     ACTIVACION_EXPERIMENTOY, juego, math.radians(90))
+            juego.experimento = 0
+
+        elif (juego.vasos.vaso1):
+            print("Vamos a por el vaso 1, hay que avanzar")
+            ejecutor(VASO_1X, VASO_1Y, juego, 0)
+            juego.vasos.vaso1
     else:
         print("Rapido pirate")
-        # ejecutor(Aparcar, juego)
-    if juego.vasos.vaso1:
-        print("Vamos a por el vaso 1, hay que avanzar")
-        ejecutor(VASO_1X, VASO_1Y, juego)
-    juego.tiempo = juego.tiempo+1
+        ejecutor(PUERTO_SUR_GRISX, PUERTO_SUR_GRISY, juego, -180)
+    print("He salido")
 
 
 # Ejecutor recrea TyM parcialmente, es decir recibe que ha de hacer y lo traduce en  instrucciones
-def ejecutor(Objx, Objy, juego):
+def ejecutor(Objx, Objy, juego, orientacion_final):
     # (Falta por implementar la creación de los waypoints, asi que solo hace gira->avanza)
-    v, b = calculo_giro_avanzo(
-        juego.robot.pos[0], juego.robot.pos[1], Objx, Objy, juego.robot.orientacion)
-    print(v)
-    print(b)
-    feeback = control(v, b)
-    if(feeback):
+    distancia, angulo_giro1, angulo_giro2 = calculo_giro_avanzo_giro(
+        juego.robot.pos[0], juego.robot.pos[1], Objx, Objy, juego.robot.orientacion, orientacion_final)
+    print(distancia)
+    print(angulo_giro1)
+    print(angulo_giro2)
+    feedback = control(distancia, angulo_giro1+angulo_giro2, juego)
+    if(feedback):
         juego.robot.pos[0] = Objx
         juego.robot.pos[1] = Objy
+        juego.robot.orientacion = orientacion_final
+        return 1
     else:
         print("Error")
-
+        return 0
     # Simula el nivel bajo
     # def control(juego, instrucciones)
 
@@ -98,6 +140,8 @@ def main():
     # vaso1 = vaso(1, 15, 2, 'r')
     while juego.tiempo < 140:
         planificador(juego)
+        print("Vamos por el segundo de partido:")
+        print(juego.tiempo)
         # print(juego.campo.vaso1.color)
         # print(juego.campo)
 
--- funcionesTyM.py
+++ funcionesTyM.py
@@ -1,9 +1,9 @@
 import math
 
 
-def calculo_giro_avanzo(posrobotx, posroboty, posobjetivox, posobjetivoy, orientacion):
+def calculo_giro_avanzo_giro(posrobotx, posroboty, posobjetivox, posobjetivoy, orientacion_original, orientacion_final):
     """
-        calculo_giro_avanzo:: Int [Posrobotx,Posroboty,Posobx,Posoby,Yaw] -> Int Angle
+        calculo_giro_avanzo:: Int [Posrobotx,Posroboty,Posobx,Posoby,Original_Yaw,Final_Yaw] -> Int Angle
         Recibe la posición del robot, su orientación y la posición del objetivo
         y calcula el ángulo de giro que ha de realizar.
         >>> calculo_giro(0,0,10,10,-45)
@@ -15,9 +15,13 @@ def calculo_giro_avanzo(posrobotx, posroboty, posobjetivox, posobjetivoy, orient
     """
     distancia_x = posobjetivox-posrobotx
     distancia_y = posobjetivoy-posroboty
-    distancia_total = math.sqrt(distancia_x**2+distancia_y**2)
+    distancia_total = math.sqrt(
+        distancia_x**2+distancia_y**2)   # Calculo el módulo
+
+    # Si no es un caso especial, se aplica la arcotangente
     if(distancia_x != 0):
         angulo_objetivo = math.atan(distancia_y/distancia_x)
+    # Para los casos especiales donde la arcotangete puede dar problemas
     else:
         if(distancia_y > 0):
             angulo_objetivo = math.pi/2
@@ -25,26 +29,82 @@ def calculo_giro_avanzo(posrobotx, posroboty, posobjetivox, posobjetivoy, orient
             angulo_objetivo = 3*math.pi/2
         else:
             angulo_objetivo = 0
-
-    angulo_giro = angulo_objetivo-orientacion
-    if(abs(angulo_giro) > 180):
-        if(angulo_giro > 0):
-            angulo_giro = angulo_giro-360
+    # Uno de los ángulos de giro se calcula asi:
+    angulo_giro1 = angulo_objetivo-orientacion_original
+    if(abs(angulo_giro1) > math.pi):  # Si te has equivocado y era el otro
+        if(angulo_giro1 > 0):
+            angulo_giro1 = angulo_giro1-2*math.pi
         else:
-            angulo_giro = angulo_giro+360
-    print("Se ha de girar este angulo:")
-    print(math.degrees(angulo_giro))
+            angulo_giro1 = angulo_giro1+2*math.pi
+    # Para calcular el otro ángulo se plantea algo similar a lo anterior
+    angulo_giro2 = orientacion_final-angulo_objetivo
+    if(abs(angulo_giro2) > math.pi):
+        if(angulo_giro2 > 0):
+            angulo_giro2 = angulo_giro2-2*math.pi
+        else:
+            angulo_giro2 = angulo_giro2+2*math.pi
+    # Printeos para asegurar que todo ha ido bien
+    print("Se ha de girar este angulo primero:")
+    print(math.degrees(angulo_giro1))
     print("Y esta es la distancia que se ha de recorrer: ")
     print(distancia_total)
-    return distancia_total, angulo_giro
+    print("Por último se gira:")
+    print(angulo_giro2)
+    return distancia_total, angulo_giro1, angulo_giro2
 
 
-def instrucciones_giro_avanzo(posrobotx, posroboty, posobjetivox, posobjetivoy, orientacion):
-    distancia_total, angulo_giro = calculo_giro_avanzo(
-        posrobotx, posroboty, posobjetivox, posobjetivoy, orientacion)
-    angulo_giro = int(math.degrees(angulo_giro))
-    instruccion_giro = "G"+str(angulo_giro)
-    print(instruccion_giro)
+def instrucciones_giro_avanzo_giro(posrobotx, posroboty, posobjetivox, posobjetivoy, orientacion, orientacion_final):
+    """
+        instrucciones_giro_avanzo:: Int [Posrobotx,Posroboty,Posobx,Posoby,Yaw] -> String 
+        Recibe la posición del robot, su orientación, la posición del objetivo
+        y la orientación final y todo esto lo codifica en instrucciones para el nivel bajo
+    """
+    distancia_total, angulo_giro1, angulo_giro2 = calculo_giro_avanzo_giro(
+        posrobotx, posroboty, posobjetivox, posobjetivoy, orientacion, orientacion_final)
+    angulo_giro1 = int(math.degrees(angulo_giro1))
+    instruccion_giro1 = "G"+str(angulo_giro1)
+    print(instruccion_giro1)
     instruccion_distancia = "D"+str(int(distancia_total))
     print(instruccion_distancia)
-    return instruccion_giro, instruccion_distancia
+    angulo_giro2 = int(math.degrees(angulo_giro2))
+    instruccion_giro2 = "G"+str(angulo_giro2)
+    print(instruccion_giro2)
+    return instruccion_giro1, instruccion_distancia, instruccion_giro2
+
+
+def instrucciones_lidar(posrobotx, posroboty, poslidarx, poslidary):
+    # filtrado paredes en proceso
+    # Poder indicar la cantidad de robots del enemigo estaría guay
+    distancia_filtro = 600  # 60cm
+    radio_seguridad = 300  # 40cm
+    medio_lado_robot = 200  # 20cm
+    distancia_total = math.sqrt(poslidarx**2+poslidary**2)
+    if(distancia_total < distancia_filtro):
+        # Si no es un caso especial, se aplica la arcotangente
+        if(poslidarx != 0):
+            angulo_objeto = math.atan(poslidary/poslidarx)
+    # Para los casos especiales donde la arcotangete puede dar problemas
+        else:
+            if(poslidary > 0):
+                angulo_objeto = math.pi/2
+            else:
+                angulo_objeto = 0
+        if(angulo_objeto > 0):
+            proyeccion_de_seguridad = distancia_total * \
+                math.cos(angulo_objeto)-radio_seguridad
+            if(proyeccion_de_seguridad < medio_lado_robot):
+                return 1, 'S'  # Para, que te lo comes
+            else:
+                return 0, 'V'  # Reduce la velocidad
+        elif(angulo_objeto < 0):
+            proyeccion_de_seguridad = +radio_seguridad - \
+                distancia_total*math.cos(angulo_objeto)
+            if(proyeccion_de_seguridad > -1*medio_lado_robot):
+                return 1, 'S'  # Para, que te lo comes
+            else:
+                return 0, 'V'  # Reduce la velocidad, en el futuro dependera de la zona y el ángulo
+        else:
+            return 0, '0'
+
+    else:
+        return 0, '0'
