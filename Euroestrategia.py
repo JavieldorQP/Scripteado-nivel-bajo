a607b4c76f41496284d04c59a21fc79f5cc07c30
Javier Ortiz Perez-Jaraiz <javierortiz@edu.uah.es>
2020-02-02 16:40:13 +0100

    Más legible aun

--- Euroestrategia.py
+++ Euroestrategia.py
@@ -13,11 +13,12 @@
 # Biliotecas:
 import time
 from Simulo_robot import simula_movimiento, actuadores, camara
-# Constantes
-# Definir vaso 1 como una tupla
-# Indicar cuantos robots hay
+
+# Constantes:
+# Vasos:
 VASO_1X = 0
 VASO_1Y = 0
+# Experimento
 ACTIVACION_EXPERIMENTOX_AZUL = -1200
 ACTIVACION_EXPERIMENTOY_AZUL = 800
 ACTIVACION_EXPERIMENTOX_AMARILLO = -ACTIVACION_EXPERIMENTOX_AZUL
@@ -48,10 +49,9 @@ PUERTO_NORTE_AZULY = 550
 # Puertos Amarillos:
 PUERTO_SUR_AMARILLOX = -PUERTO_SUR_AZULX
 PUERTO_SUR_AMARILLOY = PUERTO_SUR_AZULY
-
 PUERTO_NORTE_AMARILLOX = -PUERTO_NORTE_AZULX
 PUERTO_NORTE_AMARILLOY = PUERTO_NORTE_AZULY
-# Posición para ver la camara:
+# Posición para ver con la camara:
 CAMARA_X = 0
 CAMARA_Y = 800
 # Instrucciones
@@ -71,96 +71,108 @@ AMARILLO = 1
 AZUL = 2
 
 
-class robot:  # Clase tipo robot donde se almacenan todos los valores insteresantes del propio estado del robot
+class robot:         # Clase tipo robot donde se almacenan todos los valores insteresantes del propio estado del robot:
     def __init__(self, posx, posy, orientacion):
         self.disponible = True
         self.pos = [posx, posy]
-        self.bandera = 0
         self.velocidad = [0, 0]
         self.velocidadangular = [0]
         self.orientacion = orientacion
 
 
-class Posavasos:
+class Posavasos:     # Clase que representa toda la información de posavasos:
     def __init__(self, posx, posy, orientacion):
-        self.robot = robot(posx, posy, orientacion)  # Datos por concretar
-        self.ventosas_ocupada = False  # No ha cogido ningún vaso
+        self.robot = robot(posx, posy, orientacion)                 
+        self.ventosas_ocupada = False                               # No ha cogido ningún vaso
 
 
-class Parejitas:
+class Parejitas:  # Clase que representa toda la información de parejitas
     def __init__(self, posx, posy, orientacion):
-        self.robot = robot(posx, posy, orientacion)  # Datos por concretar
-        self.compuertas = False  # Estan abiertas
-        self.actuador_banda = False
+        self.robot = robot(posx, posy, orientacion)  
+        self.compuertas = False                                     # Estan abiertas
+        self.actuador_manga = False                                 # Actuador para tirar las mangas de viento
+        self.bandera = 0                                            # Ha sacado ya la bandera
 
 
-class vasos():  #
+class vasos:         # Almacena la información de la disponibilidad de los vasos
     def __init__(self, vaso1, vaso2, vaso3, vaso4):
-        self.vaso1 = vaso1
+        self.vaso1 = vaso1                                          
         self.vaso2 = vaso2
         self.vaso3 = vaso4
         self.vaso4 = vaso4
 
 
-class estanterias():  #
-    def __init__(self, estanteria_casa, estanteria_neutro_cerca, estanteria3, estanteria4):
-        self.estanteria_casa = estanteria_casa
-        self.estanteria_neutro_cerca = estanteria_neutro_cerca
-        self.estanteria3 = estanteria3
-        self.estanteria4 = estanteria4
+class estanterias:   # Almacena la información de las estanterias y si esta disponible
+    def __init__(self, estanteria_casa, estanteria_neutro_cerca, estanteria_neutro_lejos, estanteria_enemigo):
+        self.estanteria_casa = estanteria_casa                      # Disponibilidad de mi lado del mapa
+        self.estanteria_neutro_cerca = estanteria_neutro_cerca      # Disponibilidad de la estanteria más cercana a casa del neutro
+        self.estanteria_neutro_lejos = estanteria_neutro_lejos      # Disponibilidad de la estanteria más cercana a casa del neutro
+        self.estanteria_enemigo = estanteria_enemigo                # Disponibilidad de la estanteria del enemigo
 
 
-class game:  # Clase tipo game, donde se almacenan toda la información del partido y con un vistazo obtienes toda la información
+class game:          # Clase tipo game, donde se almacenan toda la información del partido y con un vistazo obtienes toda la información
     def __init__(self, lado, pos_inicio_parejitasx, pos_inicio_parejitasy, pos_inicio_posavasosx, pos_inicio_posavasosy, orientacion_inicial):
-        self.Activo = True
-        self.tiempo = 0
-        self.puntos = 0
-        self.lado = lado
-        self.posavasos = Posavasos(
+        self.Activo = True                                          # El partido esta activo
+        self.tiempo = 0                                             # El segundo de partido por el que vamos
+        self.puntos = 0                                             # Los puntos que tenemos
+        self.lado = lado                                            # Nuestro lado, puede ser el azul o el amarillo
+        self.posavasos = Posavasos(                                 # La clase posavasos
             pos_inicio_posavasosx, pos_inicio_posavasosy, orientacion_inicial)
-        self.parejitas = Parejitas(
+        self.parejitas = Parejitas(                                 # La clase parejtas
             pos_inicio_parejitasx, pos_inicio_parejitasy, orientacion_inicial)
-        self.vasos = vasos(True, True, True, True)
-        self.estanterias = estanterias(True, True, True, True)
-        self.experimento = True
-        self.brujula = "D"
+        self.vasos = vasos(True, True, True, True)                  # Disponibilidad de los vasos
+        self.estanterias = estanterias(True, True, True, True)      # Disponibilidad de las estanterias
+        self.experimento = True                                     # Se puede activar el experimento
+        self.brujula = "D"                                          # Color de la brujula desconocida
 
-
-# Revisa el estado de la clase juego y decide que hacer, hecho esto cede el contro a ejecutor
 
 
 def planificador(juego):
-    if(juego.tiempo <= 90):  # Hay tiempo
-        if(juego.posavasos.robot.disponible):  # Posavasos no esta ocupado
-            if(juego.experimento):
+    """
+    Planificador:
+        Esta función se encarga de mirar el estado de juego y en función de eso 
+        decide que debe de hacer el robot, una vez lo decide le cede el control
+        a ejecutor.
+    """
+    if(juego.tiempo <= 90):                                         # Si hay tiempo pensamos en que se puede hacer
+        if(juego.posavasos.robot.disponible):                       # Si posavasos no esta ocupado busca que orden mandarle
+            if(juego.experimento):                                  # Activar el experimento es prioritario y coincide con la ruta de posavasos
                 print("Activo el experimento")
-                ejecutor(ACTIVAR_EXPERIMENTO, POSAVASOS, juego)
-            elif(juego.posavasos.ventosas_ocupada):
+                ejecutor(ACTIVAR_EXPERIMENTO, POSAVASOS, juego)           
+            elif(juego.posavasos.ventosas_ocupada):                 # Si tienes el acutador ocupado lo prioritario es soltar los vasos en nuestra bahia
                 print("Vamos a la bahia a descargar los vasos")
                 ejecutor(BAHIA_SOLTAR, POSAVASOS, juego)
-            elif (juego.estanterias.estanteria_neutro_cerca):
+            elif (juego.estanterias.estanteria_neutro_cerca):       # Si aun estan disponibles los vasos en el neutro lo recomendable es ir a por ello
                 print("Voy a la estanteria del medio")
                 ejecutor(ESTANTERIAS_NEUTRO_CERCA, POSAVASOS, juego)
             elif (juego.estanterias.estanteria_casa):
-                print("Voy a la estanteria de mi lado")
+                print("Voy a la estanteria de mi lado")             # Una vez has recogido el resto de estanterias, hay que ir a la de nuestra casa
                 ejecutor(ESTANTERIAS_CERCA, POSAVASOS, juego)
-            elif (juego.brujula == 'N'):
+            elif (juego.brujula == 'N'):                            # Si no es conocida la posición de la brújula y no tienes nada más que hacer ve a revisarla
                 print("Voy a mirar la brujula")
                 ejecutor(ACTUALIZAR_BRUJULA, POSAVASOS, juego)
-            else:
+            else:                                                   # Si ya no tiene nada más que hacer que vuelva a casa para asegurar los puntos de anclaje
                 print("No tengo que hacer nada")
                 ejecutor(CASA, POSAVASOS, juego)
     # if(juego.parejitas.disponible):
     #    print("Parejitas es tu turno")
-    else:
+    else:                                                           # No queda tiempo para hacer nada más ambos robots se tienen que ir a casa para puntuar los puntos de anclaje
         print("Rapido pirate")
         ejecutor(CASA, POSAVASOS, juego)
         ejecutor(CASA, PAREJITAS, juego)
 
 
-# Ejecutor recrea TyM parcialmente, es decir recibe que ha de hacer y lo traduce en  instrucciones
 def ejecutor(orden, robot, juego):
-    # (Falta por implementar la creación de los waypoints, asi que solo hace gira->avanza)
+    """
+    Ejecutor:
+        Esta función recibe una orden de planificador y se la manda a la MDK2, una vez la 
+        MDK2 ha realizado la orden, ejecutor se encarga de actualizar el estado de juego
+        y espera a que planificador le mande otra orden.
+        Para simular la actuación de la MDK2 se crean distintas funciones que estan en
+        la biblioteca Simulo_robot.
+
+    """
+
     if(orden == ACTIVAR_EXPERIMENTO):
         if(juego.lado == AMARILLO):
             print("Activo faro amarillo")
@@ -251,7 +263,8 @@ def ejecutor(orden, robot, juego):
 
 
 def main():
-    print("Selecciona el lado (1 para amarillo y 2 para el Azul ):")
+    #Selección del lado y configuración en función del valor escaneado.
+    print("Selecciona el lado (1 para amarillo y 2 para el Azul ):")             
     lado = int(input())
     if(lado == AMARILLO):
         print("AMARILLO")
@@ -269,13 +282,13 @@ def main():
     print("Posicion de posavasos")
     print(juego.posavasos.robot.pos[0])
     print(juego.posavasos.robot.pos[1])
-    time.sleep(3)
-    while juego.Activo:
 
-        planificador(juego)
-        print("Vamos por el segundo de partido:")
+    while juego.Activo:                                             # Mientras  el juego este en su trancurso
+        planificador(juego)                                         
+        print("Vamos por el segundo de partido:")       
         print(juego.tiempo)
-
+        
+    #Una vez acabado se resume
     print("Partido acabado")
     print("Resumen:")
     print("Ha durando tantos segundos:")
