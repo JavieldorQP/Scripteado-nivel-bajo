7a14ccee7c0f873918c74018574dc80b26bb4a8d
Trompiz <javier.ortiz@edu.uah.es>
2020-02-03 10:39:29 +0100

    Comunicación UART_Miguel

---
+++ UART_ACTUADORES/UART/Uart_actuadores.h
@@ -0,0 +1,48 @@
+#ifndef UART_H_
+#define UART_H_
+
+// Accepted Error baud rate value (in percent unit)
+
+//Parametros inamovibles comunicación:
+#define UART_ACCEPTED_BAUDRATE_ERROR 3
+#define CHAR_8_BIT (3 << 0)
+#define STOP_1_BIT (0 << 2)
+#define PARITY_NONE (0 << 3)
+#define DLAB_ENABLE (1 << 7)
+#define FIFO_ENABLE (1 << 0)
+#define RBR_IRQ_ENABLE (1 << 0)
+#define THRE_IRQ_ENABLE (1 << 1)
+#define UART_LSR_THRE (1 << 5)
+#define RDA_INTERRUPT (2 << 1)
+#define CTI_INTERRUPT (6 << 1)
+//Parametros de posición:
+//Generales
+//#define BIT_PRIO buffer[0]
+#define T_INSTRUCCION buffer[0] //Primer char, es el que identifica la instrucción
+//Argumentos
+#define BIT_SIGNO_DISTANCIA buffer[1]
+#define BIT_C_DISTANCIA buffer[2]
+#define BIT_D_DISTANCIA buffer[3]
+#define BIT_U_DISTANCIA buffer[4]
+
+
+extern char *ptr_rx;     // Puntero de recepción
+extern char rx_completa; // Flag de recepción de cadena que se activa a "1" al recibir la tecla return CR(ASCII=13)
+extern char *ptr_tx;     // Puntero de transmisión
+extern char tx_completa; // Flag de transmision completa
+extern char buffer[70];  // Buffer de recepción de caracteres
+
+extern char estado;
+extern int distancia;
+extern int velocidad;
+extern int velocidad_maxima;
+extern int radio;
+extern int grados_giro;
+extern char Instruccion_Codigo;
+extern char Instruccion_Prioridad;
+
+extern void uart0_init(int baudrate);
+extern void transmitir_cadenaUART0(char *cadena);
+extern void Traduccion_Variables(void);
+
+#endif /* UART_H_ */
---
+++ UART_ACTUADORES/UART/eurouart.d
@@ -0,0 +1,8 @@
+.\eurouart.o: Eurouart.c
+.\eurouart.o: Eurouart.h
+.\eurouart.o: C:\Keil\ARM\Inc\NXP\LPC17xx\LPC17xx.h
+.\eurouart.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
+.\eurouart.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\eurouart.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\eurouart.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\eurouart.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
