18d94d4c653819b820a6f379d8d952e4558f0704
Trompiz <javier.ortiz@edu.uah.es>
2020-02-19 18:57:37 +0100

    adkjadslkdsa

--- Simulación_Vice_Raspy/mando_instrucciones.py
+++ Simulación_Vice_Raspy/mando_instrucciones.py
@@ -1,7 +1,3 @@
 from Eurocomunicacion_raspy_funciones import envio_instrucciones_traccion
-import math
 
 envio_instrucciones_traccion("0G-033","0G+000","0G+000")
-
-#angulo = math.degrees(math.atan(-90/-90))
-#print (f"prueba{angulo}")
